2003be960a81ab0aa4fc0795f545acff2019da67
Yushajanardhana3542 <Yushajanardhana3542@conestogac.on.ca>
2026-01-17 20:02:13 -0500

    dBank decentralized banking application

--- src/dbank_backend/main.mo
+++ src/dbank_backend/main.mo
@@ -1,5 +1,49 @@
-persistent actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
-  };
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+
+persistent actor dbank{
+  stable var currentValue: Float = 300.0;
+
+  stable var startTime = Time.now();
+  Debug.print(debug_show(startTime));
+
+  public func topUP(amount: Float): async() {
+    currentValue += amount;
+    Debug.print(debug_show (currentValue));
+    
+    };
+
+    public func withDrawl(amount: Float): async (){
+      let tempValue: Float = currentValue - amount;
+      if (tempValue >= 0) {
+        currentValue -= amount;
+        Debug.print(debug_show (currentValue));
+      } else 
+      Debug.print("Amount is too large current value is less tahn 0");
+
+    };
+
+
+  public query func checkBalance() : async Float {
+  currentValue;
 };
+  
+  public func compound(): async (){
+  let currentTime : Int = Time.now();
+  let elapsedNs : Int = currentTime - startTime;
+
+  // convert ns to seconds
+  let elapsedS : Int = elapsedNs / 1_000_000_000;
+
+  if (elapsedS > 0) {
+    let ratePerSecond : Float = 0.000001; 
+    let factor : Float = Float.pow(1.0 + ratePerSecond, Float.fromInt(elapsedS));
+    currentValue := currentValue * factor;
+    startTime := currentTime;
+  };
+  };
+
+
+}
--- src/dbank_frontend/src/App.jsx
+++ src/dbank_frontend/src/App.jsx
@@ -1,31 +1,87 @@
-import { useState } from 'react';
-import { dbank_backend } from 'declarations/dbank_backend';
+import React, {useState, useEffect} from "react";
+import { dbank_backend } from "../../declarations/dbank_backend";
 
-function App() {
-  const [greeting, setGreeting] = useState('');
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    dbank_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
+export default function App() {
+
+  const [balance, setBalance] = useState("");
+  const [topUpAmount, setTopUpAmount] = useState("");
+  const [withdrawAmount, setWithdrawAmount] = useState("");
+  const [loading, setLoading] = useState(false);
+
+
+  async function refreshBalance() {
+    const balance = await dbank_backend.checkBalance();
+    setBalance((await balance).toFixed(2))
+  }
+  useEffect(() => {
+    refreshBalance();
+  }, []);
+
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const top = parseFloat(topUpAmount);
+      const wd = parseFloat(withdrawAmount);
+      
+      
+      await dbank_backend.compound();
+
+     
+      if (!Number.isNaN(top) && top > 0) {
+        await dbank_backend.topUP(top);
+      }
+
+      
+      if (!Number.isNaN(wd) && wd > 0) {
+        await dbank_backend.withDrawl(wd);
+      }
+      await refreshBalance();
+      setTopUpAmount("");
+      setWithdrawAmount("");
+
+     
+    } catch (err) {
+      console.error(err);
+      alert("Transaction failed. Check console.");
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
+    <div className="container">
+      <img src="/dbank_logo.png" alt="DBank logo" width="120" />
+      <h1>Current Balance: ${balance}</h1>
+
+      <div className="divider"></div>
+
+      <form onSubmit={handleSubmit}>
+        <h2>Amount to Top Up</h2>
+        <input
+          type="number"
+          step="0.01"
+          min="0"
+          value={topUpAmount}
+          onChange={(e) => setTopUpAmount(e.target.value)}
+        />
+
+        <h2>Amount to Withdraw</h2>
+        <input
+          type="number"
+          step="0.01"
+          min="0"
+          value={withdrawAmount}
+          onChange={(e) => setWithdrawAmount(e.target.value)}
+        />
+
+<button id="submit-btn" type="submit" disabled={loading}>
+  {loading ? "Processing..." : "Finalise Transaction"}
+</button>
       </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    </div>
   );
 }
-
-export default App;
